e1f6055fc3bb59c8079b6af04b02ca34e148929c
Thomas Thornton <>
2021-11-12 14:40:49 +0000

    fix var

--- route_table.tf
+++ route_table.tf
@@ -7,7 +7,7 @@ resource "azurerm_route_table" "route_table" {
 resource "azurerm_route" "default_route" {
   name                   = var.route_name
   route_table_name       = azurerm_route_table.route_table.name
-  resource_group_name    = var.resource_group_name
+  resource_group_name    = var.virtual_network_resource_group
   address_prefix         = var.route_address_prefix
   next_hop_type          = var.route_next_hop_type
   next_hop_in_ip_address = var.route_next_hop_in_ip_address
@@ -16,7 +16,7 @@ resource "azurerm_route" "default_route" {
 resource "azurerm_route" "azure_control_plane" {
   name                = "azure-control-plane"
   route_table_name    = azurerm_route_table.route_table.name
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.virtual_network_resource_group
   address_prefix      = "51.145.56.125/32"
   next_hop_type       = "Internet"
 }
