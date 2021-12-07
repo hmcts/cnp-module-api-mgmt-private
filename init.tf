f2758c9bfd5c044b33570809bc9603260a180f4b
thomast1906 <thomast@kainos.com>
2021-12-07 15:33:10 +0000

    Add cft preview/dev

--- init.tf
+++ init.tf
@@ -51,6 +51,9 @@ locals {
     test = {
       subscription = "8a07fdcd-6abd-48b3-ad88-ff737a4b9e3c"
     }
+    dev = {
+      subscription = "8b6ea922-0862-443e-af15-6056e1c9b9a4"
+    }
   }
 
   acmedtssdsapps = {
