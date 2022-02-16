af364a08b14f19f6af7a546fb9a1274a0b1706d1
thomast1906 <thomast@kainos.com>
2022-02-16 11:25:12 +0000

    initial push to test

--- main.tf
+++ main.tf
@@ -15,24 +15,47 @@ resource "azurerm_public_ip" "apim" {
 
 }
 
-resource "azurerm_template_deployment" "apim" {
-  name                = "core-infra-subnet-apimgmt-${local.environment}"
-  resource_group_name = var.virtual_network_resource_group
-  deployment_mode     = "Incremental"
-  template_body       = file("${path.module}/arm/apim.json")
-  parameters = {
-    name                    = local.name
-    location                = var.location
-    sku_name                = var.sku_name
-    publisherEmail          = var.publisher_email
-    publisherName           = var.publisher_name
-    subnetResourceId        = data.azurerm_subnet.api-mgmt-subnet.id
-    notificationSenderEmail = var.notification_sender_email
-    virtualNetworkType      = var.virtual_network_type
-    publicIpAddressId       = azurerm_public_ip.apim.id
-    environment             = [for x in keys(local.environment_mapping) : x if contains(local.environment_mapping[x], var.environment)][0]
-    criticality             = local.criticality[var.environment]
+# resource "azurerm_template_deployment" "apim" {
+#   name                = "core-infra-subnet-apimgmt-${local.environment}"
+#   resource_group_name = var.virtual_network_resource_group
+#   deployment_mode     = "Incremental"
+#   template_body       = file("${path.module}/arm/apim.json")
+#   parameters = {
+#     name                    = local.name
+#     location                = var.location
+#     sku_name                = var.sku_name
+#     publisherEmail          = var.publisher_email
+#     publisherName           = var.publisher_name
+#     subnetResourceId        = data.azurerm_subnet.api-mgmt-subnet.id
+#     notificationSenderEmail = var.notification_sender_email
+#     virtualNetworkType      = var.virtual_network_type
+#     publicIpAddressId       = azurerm_public_ip.apim.id
+#     environment             = [for x in keys(local.environment_mapping) : x if contains(local.environment_mapping[x], var.environment)][0]
+#     criticality             = local.criticality[var.environment]
+#   }
+# }
+
+resource "azurerm_api_management" "apim" {
+  name                      = local.name
+  location                  = var.location
+  resource_group_name       = var.vnet_rg_name
+  publisher_name            = var.publisher_name
+  publisher_email           = var.publisher_email
+  notification_sender_email = var.notification_sender_email
+  virtual_network_type      = var.virtual_network_type
+
+  virtual_network_configuration {
+    subnet_id = data.azurerm_subnet.api-mgmt-subnet.id
   }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  zones               = local.zones
+  public_ip_addresses = azurerm_public_ip.example.id
+
+  sku_name = local.sku_name
 }
 
 resource "azurerm_role_assignment" "apim" {
