77750305cb4a4849f8b688677cf07335c2934740
Joshua Glass <joshua.glass@justice.gov.uk>
2024-05-07 15:09:04 +0100

    removed changes

--- temp_resources.tf
+++ temp_resources.tf
@@ -1,26 +1,26 @@
 // temp resources
 // use iaas for cft-api-mgmt-sbox only
 data "azurerm_subnet" "temp_subnet" {
-  count                = (contains([var.migration_env], var.environment) ? 1 : 0)
+  count                = (contains(["sbox"], var.environment) ? 1 : 0)
   name                 = "iaas"
   virtual_network_name = var.virtual_network_name
   resource_group_name  = var.virtual_network_resource_group
 }
 
 resource "azurerm_subnet_network_security_group_association" "apim_temp" {
-  count                     = (contains([var.migration_env], var.environment) ? 1 : 0)
+  count                     = (contains(["sbox"], var.environment) ? 1 : 0)
   subnet_id                 = data.azurerm_subnet.temp_subnet[0].id
   network_security_group_id = azurerm_network_security_group.apim.id
 }
 
 resource "azurerm_subnet_route_table_association" "temp_subnet" {
-  count          = (contains([var.migration_env], var.environment) ? 1 : 0)
+  count          = (contains(["sbox"], var.environment) ? 1 : 0)
   subnet_id      = data.azurerm_subnet.temp_subnet[0].id 
   route_table_id = azurerm_route_table.route_table.id
 }
 
 resource "azurerm_public_ip" "temp_pip" {
-  count               = (contains([var.migration_env], var.environment) ? 1 : 0)
+  count               = (contains(["sbox"], var.environment) ? 1 : 0)
   name                = "${var.department}-api-mgmt-${var.environment}-private-pip-temp"
   resource_group_name = var.virtual_network_resource_group
   location            = var.location
