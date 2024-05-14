9cd0f5a4dddedbd0acd8e8aa03557598fcc2192d
Joshua Glass <joshua.glass@justice.gov.uk>
2024-05-14 11:44:11 +0100

    test adding ithc to contains logic

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "azurerm_api_management" "apim" {
 
   zones                = local.zones
   public_ip_address_id = var.trigger_migration == true ? azurerm_public_ip.temp_pip[0].id : (
-        var.sku_name == "Premium" || contains(["sbox"], var.environment) ? azurerm_public_ip.apim.id : null )
+        var.sku_name == "Premium" || contains(["sbox", "ithc"], var.environment) ? azurerm_public_ip.apim.id : null )
 
   sku_name             = local.sku_name
 
