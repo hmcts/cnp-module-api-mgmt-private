13045ea9fdc99cd96decf6ab57e65a75aa4c383c
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2024-08-15 15:57:54 +0100

    Fix duplciate resource error

--- main.tf
+++ main.tf
@@ -94,3 +94,63 @@ resource "azurerm_api_management_custom_domain" "api-management-custom-domain" {
     azurerm_role_assignment.apim
   ]
 }
+
+resource "azurerm_api_management_diagnostic" "apim" {
+  identifier               = "applicationinsights"
+  resource_group_name      = var.virtual_network_resource_group
+  api_management_name      = azurerm_api_management.apim.name
+  api_management_logger_id = azurerm_api_management_logger.apim.id
+
+  sampling_percentage       = 5.0
+  always_log_errors         = true
+  log_client_ip             = true
+  verbosity                 = "error"
+  http_correlation_protocol = "W3C"
+
+  frontend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
+
+  frontend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
+
+  backend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
+
+  backend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
+}
+
+resource "azurerm_api_management_logger" "apim" {
+  name                = "${local.name}-logger"
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = var.virtual_network_resource_group
+  resource_id         = module.application_insights.id
+
+  application_insights {
+    instrumentation_key = module.application_insights.instrumentation_key
+  }
+}
