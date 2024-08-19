4be445c6c9221323ff368fa174162066abdb29cf
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2024-08-19 16:39:50 +0100

    Clone SDT APIM diagnostic logging

--- variables.tf
+++ variables.tf
@@ -39,12 +39,33 @@ variable "route_next_hop_in_ip_address" {
 }
 
 variable "additional_routes_apim" {
-  description = "A list of additional route configurations"
-  type = list(object({
-    name                   = string
-    address_prefix         = string
-    next_hop_type          = string
-    next_hop_in_ip_address = string
-  }))
-  default = []
+description = "A list of additional route configurations"
+type = list(object({
+name = string
+address_prefix = string
+next_hop_type = string
+next_hop_in_ip_address = string
+}))
+default = []
+}
+
+variable "aks_subscription_id" {
+  type        = string
+  description = "The AKS subscription id for the environment.  Set by pipeline."
+}
+
+variable "apim_suffix" {
+  type        = string
+  description = "The suffix to use for the APIM name.  If not set then the environment name will be used."
+  default     = ""
+}
+
+variable "product" {
+  type        = string
+  description = "The value of the product variable that is defined in the Jenkinsfile.  Set by pipeline."
+}
+
+variable "component" {
+  type        = string
+  description = "The value of the component variable that is defined in the Jenkinsfile.  Set by pipeline."
 }
