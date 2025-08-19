392c0a8fd78afd0dfda2808b7ca5e92cadf8ae58
Daniel Alves <daniel.alves@justice.gov.uk>
2025-08-19 09:19:27 +0100

    Revert "chore(azurerm): use app insights module version 4.0.0"
    This reverts commit c33ac7f7226ad6f90a4985a6024087f249fb2177.

--- app-insights.tf
+++ app-insights.tf
@@ -1,5 +1,5 @@
 module "application_insights" {
-  source = "git::https://github.com/hmcts/terraform-module-application-insights?ref=4.x"
+  source = "git::https://github.com/hmcts/terraform-module-application-insights?ref=main"
 
   env     = local.environment
   product = var.department
